a1126b8baad75a67f7c5ef679580c4f783abcc86
Tanish Kothari <tanishkothari@Tanishs-MacBook-Air-2.local>
2024-08-14 00:31:17 +0530

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello World</h1>
+        
       </header>
     </div>
   );
